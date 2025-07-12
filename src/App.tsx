03a7aaa755524f1393a7c25c3b9bd68f47f78c17
Thushara R Shenoi <thushara.r.shenoi@pwc.com>
2025-07-12 20:50:39 +0530

    Employer mock up added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Analysis from "./pages/Analysis";
-import Results from "./pages/Results";
+import Results from "./pages/Results";//results page
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login"; // Import the Login component
